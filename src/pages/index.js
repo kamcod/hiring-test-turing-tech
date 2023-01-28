baa513c0d809185bb30abb3db460f94f70598f0a
kamcod <mkamran4786@gmail.com>
2023-01-28 19:45:03 +0500

    home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,13 @@
+import {useEffect} from "react";
+import {useRouter} from "next/router";
+import Header from "@/components/Header";
+import HomePage from "@/components/HomePage";
 
 export default function Home() {
   return (
     <>
-      Home Page
+      <Header loginPage={false}/>
+        <HomePage />
     </>
   )
 }
